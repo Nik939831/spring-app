e21f2ff2b4fa6a71c048f49fd24ae48cb661bbcf
Nik <nik939831@gmail.com>
2021-03-03 15:32:37 -0600

    initial commit

---
+++ src/main/java/com/company/training/SpringBootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.company.training;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+
+
+}
---
+++ src/test/java/com/company/training/SpringBootDemoApplicationTest.java
@@ -0,0 +1,14 @@
+package com.company.training;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpringBootDemoApplicationTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
